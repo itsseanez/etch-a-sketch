6c2a177e43ed521a723cc773653855fc1ad0aa6a
itsseanez <74938477+itsseanez@users.noreply.github.com>
2025-11-20 12:02:34 -0500

    Create createGrid method that creates a 2 dimensional array based on a given size parameter

---
+++ script.js
@@ -0,0 +1,20 @@
+const container = document.querySelector('#container');
+let createGrid = (size) => {
+    let grid = [];
+
+    for (i = 0; i < size; i++) {
+        let row = document.createElement('div');
+        row.classList.add("row");
+        container.appendChild(row);
+        grid[i] = row;
+
+        for (j = 0; j < size; j++) {
+            let column = document.createElement("div");
+            column.classList.add("column");
+            row.appendChild(column);
+            grid[i][j] = column;
+        }
+    }
+    return grid;
+};
+console.log(createGrid(6))
